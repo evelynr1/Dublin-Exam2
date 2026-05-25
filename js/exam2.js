a9d5f0a6ac34dec25f3ce37e4a0766a50a7c8275
evelynr1 <evelynr@umich.edu>
2026-05-25 06:49:15 -0400

    answered all but part 3 question 8

--- js/exam2.js
+++ js/exam2.js
@@ -1,26 +1,39 @@
+//resources
+//  - https://github.com/UMSIComplexWebDesign/practice3 (used my answers to the prectice exam for questions 3, 4, assistance with 5)
+//  - https://www.w3schools.com/css/css_background_image.asp (used to remember the backgroundImage attribute for question 7)
 
 // Part 3 Question 3
-
-
+window.addEventListener("load",function(){
+     console.log("Evelyn");
+ });
 
 
 // Part 3 Question 4
+document.querySelector("h1").addEventListener("click",function(){
+    this.style.display = "none";
+ });
 
 
-
+// Part 3 Question 4
+document.querySelector(".partA a:nth-of-type(3) img").addEventListener("mouseover",function(){
+    this.style.border = "15px solid green";
+ });
 
 
 // Part 3 Question 6
-
-
-
+document.querySelector(".style").addEventListener("focus",function(){
+    this.style.backgroundColor = "white";
+ });
+document.querySelector(".style").addEventListener("blur",function(){
+    this.style.backgroundColor = "bisque";
+ });
 
 
 // Part 3 Question 7
-
-
-
-
+document.querySelector(".style2").addEventListener("focus",function(){
+    document.querySelector("#maincontent").style.backgroundImage = "url('images/beach.jpg')";
+ });
 
 
 // Part 3 Question 8
+
